20fcf5ed54e7eba1c6c9a998bfbc459c2ece80a1
Arthur Vincent Simon <developarvin@gmail.com>
2024-06-07 10:51:52 +0800

    feat: Add Billboard component and update layout

---
+++ src/app/_components/Billboard.tsx
@@ -0,0 +1,14 @@
+export function Billboard() {
+  return (
+    <div className={'w-8/12 text-center mx-auto p-8'}>
+      <div className={'text-6xl p-4'}>
+        Rapidly build modern websites without ever leaving your HTML.
+      </div>
+      <div className="text-viki-blue-400">
+        A utility-first CSS framework packed with classes like <span>flex</span>
+        , <span>pt-4</span>, <span></span> text-center and rotate-90 that can be
+        composed to build any design, directly in your markup.
+      </div>
+    </div>
+  )
+}
