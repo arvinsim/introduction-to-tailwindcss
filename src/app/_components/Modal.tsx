d117b1c5d1535b9f60c0df2dc1eb41eb0eed3572
Arthur Vincent Simon <developarvin@gmail.com>
2024-06-14 14:19:36 +0800

    feat: Add chat.svg icon and use it in Modal component

--- src/app/_components/Modal.tsx
+++ src/app/_components/Modal.tsx
@@ -1,5 +1,6 @@
-'use clien'
+'use client'
 import React from 'react'
+import Image from 'next/image'
 
 interface ModalProps {
   isOpen: boolean
@@ -12,7 +13,16 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
       <div className="bg-white p-8 rounded shadow-md w-1/2 text-black">
-        <h2 className="text-2xl mb-4">Hello World</h2>
+        <h2 className="text-2xl mb-4">
+          <Image
+            src={'/chat.svg'}
+            alt="Chat Icon"
+            width={36}
+            height={36}
+            className={'inline-block'}
+          />
+          Hello World
+        </h2>
         <p className="mb-4">This is a simple modal using TailwindCSS.</p>
         <button
           className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
